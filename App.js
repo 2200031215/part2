514a3add0c1625e39087f0d1e6b7cee629354ac0
2200031215 <147698689+2200031215@users.noreply.github.com>
2023-10-12 14:12:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ParentComponent from './components/ParentComponent'
+
+function App() {
+  return (
+    <div>
+      <h1>Props Example</h1>
+      <ParentComponent/>
+      
+    </div>
+  )
+}
+export default App;
